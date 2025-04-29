b04ad96cb1654eec77c1e67c054616927c668147
Sawjal <mdsajalsikder@gmai.com>
2025-04-29 17:47:49 +0600

    For admin design issue

--- interior_backend/settings.py
+++ interior_backend/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,14 +120,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'  
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # <- your app static files here (must be separate)
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <- Django will collect admin static files here
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
--- interior_backend/urls.py
+++ interior_backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +11,8 @@ urlpatterns = [
     path('home/', include('home.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
